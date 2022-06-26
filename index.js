7985bf1a350d5dfec444f90e558d23f9085c11c8
CryptoElit <heinzgericke@outlook.com>
2022-06-26 15:23:22 +0200

    added Keystrokes + animation

--- index.js
+++ index.js
@@ -5,9 +5,73 @@
 for (var i=0; i<document.querySelectorAll(".drum").length; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        
-        alert("I got clicked.");
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+    });
+}
+
+document.addEventListener("keypress", function(event) {
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+
 });
 
+function makeSound(key) {
 
-};
+    let soundMaker = new Audio();
+
+
+    switch (key) {
+        case "w":
+           soundMaker.src = "sounds/tom-1.mp3";
+        break;
+        case "a":
+            soundMaker.src = "sounds/tom-2.mp3";
+        break;
+        case "s":
+            soundMaker.src = "sounds/tom-3.mp3";
+        break;
+        case "d":
+            soundMaker.src = "sounds/tom-4.mp3";
+        break;
+        case "j":
+            soundMaker.src = "sounds/snare.mp3";
+        break;
+        case "k":
+            soundMaker.src = "sounds/kick.mp3";
+            break;
+        case "l":
+            soundMaker.src = "sounds/drum.mp3";
+        break;
+        }
+        soundMaker.play();
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+
+
+   
+       
+    
+
+
+
+
+ 
+
